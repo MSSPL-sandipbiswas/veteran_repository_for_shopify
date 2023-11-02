5c0f8f11da38b7b15e41efdfd7389543dc02166a
MSSPL-sandipbiswas <sandip.biswas@massoftind.com>
2023-11-02 17:32:35 +0530

    home page slider and about section

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,40 @@
 // Put your application javascript here
+jQuery(document).ready(function($) {
+
+    /*--------------------------------------------------------------
+    >>> Banner Slider
+    ----------------------------------------------------------------*/
+    if ($('.banner-section').length > 0) {
+        var swiper = new Swiper(".banner-section", {
+            parallax: true,
+            speed: 700,
+            autoplay: {
+                delay: 4500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+
+
+    gsap.utils.toArray(".hom_about_image_sec .left_image,.our_mission_image_loop figure").forEach(function(container) {
+        let image = container.querySelector("img");
+    
+        gsap.to(image, {
+            y: () => image.offsetHeight - container.offsetHeight,
+            ease: "none",
+            scrollTrigger: {
+            trigger: container,
+            scrub: true,
+            pin: false,
+            markers: false,
+            invalidateOnRefresh: true
+            },
+        }); 
+    });
+    gsap.registerPlugin(ScrollTrigger);
+
+});
